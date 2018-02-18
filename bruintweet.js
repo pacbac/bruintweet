fc0120a9f12ccd6eb2e2e6ac866be60e24a72381
Ryan Tong <ryanwrtong@gmail.com>
2018-02-17 18:58:10 -0800

    similar function added

--- bruintweet.js
+++ bruintweet.js
@@ -2,6 +2,7 @@
 var tweetJson;
 var courseList = []
 var subjectList = []
+var similarList = []
 $(document).ready(function(){
   $.getJSON("http://api.ucladevx.com/courses/all/all/", function(json){
     var courseJSON = JSON.stringify(json);
@@ -10,15 +11,11 @@ $(document).ready(function(){
       subjectList.push(courseJSON[i]["subject"])
     }
 
-<<<<<<< HEAD
-
-=======
   })
 
   $(".submit").click(function(){
     sub()
   })
->>>>>>> 8578a0036b688c5b0bdd8ccd2e9fea86424af159
 
 })
 
@@ -87,6 +84,10 @@ function getTags(input){
         keywordArr.push(keywordJSON[i][["keyword"]])
       }
       compareArr(keywordArr, courseList)
+      $(".crse").html("")
+      for ( var i = 0; i < similarList.length; i++) {
+          $(".crse").append("<p>" + similarList[i] + "</p>")
+      }
 
     },
     error: function(){
@@ -102,8 +103,51 @@ $("#linkJSON").click(function(){
 
 function compareArr(keywordArr, courseList){
   for(var i = 0; i < keywordArr.length; i++){
-    for(var i = 0; i < courseList.length; i++){
-
+    for(var j = 0; j < courseList.length;j++){
+      if(similarity(keywordArr[i],courseList[j]) > .7) {
+        similarList.push(courseList.length[j]);
+      }
     }
   }
+}
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
 }
