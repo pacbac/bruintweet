d2cee022c5d84801ff27865a60e48926a16f8443
clayton <23279139+pacbac@users.noreply.github.com>
2018-02-17 16:17:36 -0800

    Added JS files

--- bruintweet.js
+++ bruintweet.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
-  $.getJSON("api.ucladevx.com/courses/<quarter>/<subject>", function(json){
-    alert(json);
+  $.getJSON("http://api.ucladevx.com/courses/winter/computer science/", function(json){
+    alert(JSON.stringify(json));
+
   })
+
+  $.ajax({
+    url: "https://apis.paralleldots.com/v2/keywords?text=%22Prime%20Minister%20Narendra%20Modi%20tweeted%20a%20link%20to%20the%20speech%20Human%20Resource%20Development%20Minister%20Smriti%20Irani%20made%20in%20the%20Lok%20Sabha%20during%20the%20debate%20on%20the%20ongoing%20JNU%20row%20and%20the%20suicide%20of%20Dalit%20scholar%20Rohith%20Vemula%20at%20the%20Hyderabad%20Central%20University.&api_key=og78FIM6RTpSOv3kiRdHzWFstb7OdcVdEvao5jImHhQ",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Basic " + btoa("username:password"));
+    },
+    type: 'POST',
+    dataType: 'json',
+    contentType: 'application/json',
+    processData: false,
+    data: '{"foo":"bar"}',
+    success: function (data) {
+      alert(JSON.stringify(data));
+    },
+    error: function(){
+      alert("Cannot get data");
+    }
+  });
+
+  $.getJSON("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=c9mang0", function(data){
+    alert(JSON.stringify(data))
+  })
+
+  $.ajax({
+      url: "get-tweets.js",
+      dataType: "script",
+      cache: true,
+      success: function() {
+          alert("t")
+          $.getJSON("home.json", function(json){
+            alert(JSON.stringify(json))
+          })
+      }
+  });
+
 })
---
+++ get-tweets.js
@@ -0,0 +1,47 @@
+
+var express = require('express');
+var util = require('util');
+var OAuth2 = require('OAuth').OAuth2;
+var session = require('express-session')
+var https = require('https');
+var app = express();
+var fs = require('fs');
+
+var screenName = "C9Mang0"
+
+app.get('/home', function(req, res){
+  var oauth2 = new OAuth2("qKDmV0gXFjAejwY0xeQbcX6IE", "w8VMVBmq4YqTSdHyXc3dgULK7fC6p6J6OgzRjNzbgRPeWupARN", 'https://api.twitter.com/', null, 'oauth2/token', null);
+  oauth2.getOAuthAccessToken('', {
+      'grant_type': 'client_credentials'
+  }, function (e, access_token) {
+      console.log(access_token); //string that we can use to authenticate request
+
+      var options = {
+          hostname: 'api.twitter.com',
+          path: '/1.1/statuses/user_timeline.json?screen_name='+screenName,
+          headers: {
+              Authorization: 'Bearer ' + access_token
+          }
+      };
+
+
+      https.get(options, function (result) {
+          var buffer = '';
+          result.setEncoding('utf8');
+          result.on('data', function (data) {
+              buffer += data;
+          });
+          result.on('end', function () {
+              var tweets = JSON.parse(buffer);
+              res.send(tweets);
+              fs.writeFile("home.json", JSON.stringify(tweets))
+          });
+      });
+  });
+});
+
+app.get('*', function(req, res){
+    res.redirect('/home');
+});
+
+app.listen(8080);
